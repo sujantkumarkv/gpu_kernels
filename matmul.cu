a608d3032f8f321a463fd6602c1371f3756f3c98
Sujant Kumar Kv <sujantkv@gmail.com>
2023-11-07 03:43:10 +0530

    matmul v1

---
+++ matmul.cu
@@ -0,0 +1,117 @@
+#include <stdio.h>
+// cuda kernel for multiplication
+__global__ void vecmul (float *a, float *b, float *d_temp, int N) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < N)
+    {
+        d_temp[i] = a[i] * b[i];
+    }
+}
+// kernel for futher addition (reduction algo)
+__global__ void reduction(float *d_temp, int N) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < N/2)
+    {
+        d_temp[i] += d_temp[i+N/2];
+    }
+    __syncthreads();
+    if (N % 2 !=0 && i==0) {
+        d_temp[0] += d_temp[N-1];
+    }
+}
+
+int vecmul_func (float *d_A_row, float *d_B_col, float *d_C_ele, int N) {
+    float *d_temp;
+    cudaMalloc((void **)&d_temp, N * sizeof(float));
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    vecmul<<<blocksPerGrid, threadsPerBlock>>>(d_A_row, d_B_col, d_temp, N);
+
+    while (N > 1) {
+        blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+        reduction<<<blocksPerGrid, threadsPerBlock>>>(d_temp, N);
+        N = N / 2;
+    }
+    /**
+     * copying a single float value from d_temp[0] to the location pointed by d_C_ele. 
+     * This effectively stores the result of vecmul & reduction in the (i, j)-th element of matrix d_C
+     * thus, updating d_C's values with the pointer d_C_ele by default.
+    */
+    cudaMemcpy(d_C_ele, d_temp, sizeof(float), cudaMemcpyDeviceToDevice);
+    cudaFree(d_temp);
+    return 0;
+}
+int main() {
+    int P=2, Q=3, R=2;
+    float *h_A, *h_B, *h_C;     // host
+    float *d_A, *d_B, *d_C;     // device
+    // Allocate host memory
+    h_A = (float *)malloc(P * Q * sizeof(float));
+    h_B = (float *)malloc(Q * R * sizeof(float));
+    h_C = (float *)malloc(P * R * sizeof(float));
+
+    // Allocate device memory
+    cudaMalloc((void **)&d_A, P * Q * sizeof(float));
+    cudaMalloc((void **)&d_B, Q * R * sizeof(float));
+    cudaMalloc((void **)&d_C, P * R * sizeof(float));
+    // cudaMalloc((void **)&d_temp, P * R * sizeof(float));
+    // Initialize host arrays and copy to device
+    for (int i = 0; i < P; ++i) {
+        for (int j = 0; j < Q; ++j) {
+            h_A[i * Q + j] = float(i * Q + j);
+        }
+    }
+
+    for (int i = 0; i < Q; ++i) {
+        for (int j = 0; j < R; ++j) {
+            h_B[i * R + j] = float(i * R + j + 69);
+        }
+    }
+
+    for (int i = 0; i < P; ++i) {
+        for (int j = 0; j < R; ++j) {
+            h_C[i * R + j] = 0.0f; // Initialize h_C to 0 as it will hold the result
+        }
+    }
+    cudaMemcpy(d_A, h_A, P * Q * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, Q * R * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_C, h_C, P * R * sizeof(float), cudaMemcpyHostToDevice);
+
+    // invoke kernel for matrix rows & col
+    for (int i = 0; i < P; ++i) {
+        for (int j = 0; j < R; ++j) {
+            float *d_A_row = &d_A[i * Q];
+            float *d_B_col = &d_B[j];
+            float *d_C_ele = &d_C[i * R + j];
+            /**
+             * d_C_ele is not a vector, it's a pointer to a single float value in the device memory. 
+             * calling vecmul_func, we pass &d_C[i * N + j] as d_C_ele, which is (i, j)-th element in matrix C. 
+             * So, d_C_ele points to a specific location in the d_C.
+            */
+            vecmul_func(d_A_row, d_B_col, d_C_ele, Q);
+        }
+    }
+
+    // Copy result back to host
+    cudaMemcpy(h_C, d_C, P * R * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // result
+    printf("result h_C: %f\n", *h_C);
+    for (int i = 0; i < P; ++i) {
+        for (int j = 0; j < R; ++j) {
+            printf("%f ", h_C[i * R + j]);
+        }
+        printf("\n");
+    }
+    // Cleanup
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_temp);
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    return 0;
+}
+
